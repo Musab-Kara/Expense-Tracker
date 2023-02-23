4d14e53df923caf5dc9e9d7873bcf0932cf9163a
Musab Kara <msb.kara.94@gmail.com>
2023-02-22 22:54:10 -0500

    part 2 done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
+import Card from "./Card.js";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}$</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
